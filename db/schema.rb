88d3012b10356ba42f53b96451125487c199adb0
Shinnosuke Matsuo <shinnosuke1a@gmail.com>
2019-05-28 19:56:40 +0900

    view change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_085645) do
+ActiveRecord::Schema.define(version: 2019_05_26_085317) do
 
   create_table "matchings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,35 +31,10 @@ ActiveRecord::Schema.define(version: 2019_05_26_085645) do
     t.index ["room_id"], name: "index_messages_on_room_id"
   end
 
-  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_microposts_on_user_id"
-  end
-
-  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "Related_links"
-    t.string "carrer"
-    t.string "portfolio"
-    t.text "content"
-    t.index ["user_id", "created_at"], name: "index_profiles_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "role"
   end
 
 end
